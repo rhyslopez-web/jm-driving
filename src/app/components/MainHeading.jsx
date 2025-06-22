1ea84eedec91ddd5b57703a6e69cbd0067e07dee
Brittany Holmes <Brittany.Holmes@vanarts.com>
2025-06-22 13:33:39 -0700

    updated fonts and started about page

--- src/app/components/MainHeading.jsx
+++ src/app/components/MainHeading.jsx
@@ -8,7 +8,7 @@ const MainHeading = ({children, className}) => {
     <motion.h1
     initial={{opacity: 0, y:100}}
     whileInView={{opacity:1, y:0 , transition:{duration:0.5, ease: 'easeInOut'}}}
-    className={className + " font-primary leading-tight text-[clamp(2rem,6vw,3.75rem)] lg:text-6xl text-blue-primary" }
+    className={className + " font-primary leading-tight text-[clamp(2rem,6vw,3.75rem)] lg:text-6xl text-blue-primary font-medium uppercase" }
     >
         {children}
     </motion.h1>
