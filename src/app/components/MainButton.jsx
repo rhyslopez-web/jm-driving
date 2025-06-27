4a2e6eee538809d483822dfde6091b4af0db6e43
rhyslopez-web <rhyslopez.web@gmail.com>
2025-06-27 00:03:03 -0700

    added button shadow animations

--- src/app/components/MainButton.jsx
+++ src/app/components/MainButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MainButton = ({children, href, className}) => {
   return (
-    <a href={href} className={className + ' bg-blue-primary px-7 py-3 rounded-full text-white flex gap-3 items-center justify-center'}>
+    <a href={href} className={className + ' bg-blue-primary px-7 py-3 rounded-full text-white flex gap-3 items-center justify-center transition ease-in-out duration-500 hover:shadow-blue-primary hover:shadow-lg'}>
       {children}
     </a>
   )
--- src/app/components/SecondaryButton.jsx
+++ src/app/components/SecondaryButton.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const SecondaryButton = ({children, className, href}) => {
   return (
     <a href={href} 
-    className={className + ' border border-dashed border-blue-primary text-center px-10 py-3 rounded-full text-blue-primary hover:bg-blue-primary transition duration-100 ease-in-out hover:text-white'
-      
+    className={className + ' border border-dashed border-blue-primary text-center px-10 py-3 rounded-full text-blue-primary hover:bg-blue-primary transition duration-500 ease-in-out hover:text-white hover:shadow-blue-primary hover:shadow-lg'
     }>
         {children}
     </a>
