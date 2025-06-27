677dbd53f1cda33c8338fbd4ca67734ce17f317c
rhyslopez-web <rhyslopez.web@gmail.com>
2025-06-26 23:40:37 -0700

    Added smooth scrolling and edited secondary button border

--- src/app/components/SecondaryButton.jsx
+++ src/app/components/SecondaryButton.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SecondaryButton = ({children, className, href}) => {
   return (
     <a href={href} 
-    className={className + ' border border-dashed border-blue-primary text-center px-10 py-3 rounded-full text-blue-primary hover:bg-blue-primary transition duration-100 ease-in-out hover:text-white hover:border-white'
+    className={className + ' border border-dashed border-blue-primary text-center px-10 py-3 rounded-full text-blue-primary hover:bg-blue-primary transition duration-100 ease-in-out hover:text-white'
       
     }>
         {children}
